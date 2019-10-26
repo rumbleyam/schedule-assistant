db5a0330ac8ef75427dd8d5d4b6656ffd8816fa6
Andrew Rumbley <andrew@shockoe.com>
2019-10-26 18:09:33 -0400

    MVP complete.  Scheduler implemented.  Settings and polish on the schedule to come.

---
+++ src/components/Form.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import {
+  KeyboardDatePicker,
+  KeyboardTimePicker,
+} from '@material-ui/pickers';
+
+const useStyles = makeStyles(theme => ({
+  inputContainer: {
+    flexDirection: 'column',
+  },
+  caption: {
+    marginLeft: 0,
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function Form({
+  selectedDate,
+  handleDateChange,
+  weekdays,
+  handleWeekdaysChange,
+  occurrences,
+  handleOccurrenceChange
+}) {
+  const classes = useStyles();
+  return (
+    <Box display="flex" flexDirection="column" alignItems="flex-start">
+      <KeyboardDatePicker
+        margin="normal"
+        id="date-picker-dialog"
+        label="Start Date"
+        format="MM/DD/YY"
+        value={selectedDate}
+        onChange={handleDateChange}
+        KeyboardButtonProps={{
+          'aria-label': 'change date',
+        }}
+      />
+      <KeyboardTimePicker
+        margin="normal"
+        id="time-picker"
+        label="Time Slot"
+        minutesStep={5}
+        value={selectedDate}
+        onChange={handleDateChange}
+        KeyboardButtonProps={{
+          'aria-label': 'change time',
+        }}
+      />
+      <Typography variant="caption" className={classes.caption}>
+        Weekdays
+      </Typography>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Checkbox checked={weekdays.sunday} onChange={handleWeekdaysChange('sunday')} value="true" color="primary"/>
+          }
+          label="Sunday"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox checked={weekdays.monday} onChange={handleWeekdaysChange('monday')} value="true" color="primary"/>
+          }
+          label="Monday"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox checked={weekdays.tuesday} onChange={handleWeekdaysChange('tuesday')} value="true" color="primary"/>
+          }
+          label="Tuesday"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox checked={weekdays.wednesday} onChange={handleWeekdaysChange('wednesday')} value="true" color="primary"/>
+          }
+          label="Wednesday"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox checked={weekdays.thursday} onChange={handleWeekdaysChange('thursday')} value="true" color="primary"/>
+          }
+          label="Thursday"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox checked={weekdays.friday} onChange={handleWeekdaysChange('friday')} value="true" color="primary"/>
+          }
+          label="Friday"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox checked={weekdays.saturday} onChange={handleWeekdaysChange('saturday')} value="true" color="primary"/>
+          }
+          label="Saturday"
+        />
+      </FormGroup>
+      <TextField
+        id="occurrences"
+        label="Occurrences"
+        value={occurrences}
+        onChange={handleOccurrenceChange}
+        type="number"
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        margin="normal"
+      />
+    </Box>
+  );
+}
---
+++ src/components/Schedule.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+  inputContainer: {
+    flexDirection: 'column',
+  },
+  caption: {
+    marginLeft: 0,
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const ISOToWeekday = {
+  1: 'monday',
+  2: 'tuesday',
+  3: 'wednesday',
+  4: 'thursday',
+  5: 'friday',
+  6: 'saturday',
+  7: 'sunday',
+};
+
+const weekdayToISO = {
+  monday: 1,
+  tuesday: 2,
+  wednesday: 3,
+  thursday: 4,
+  friday: 5,
+  saturday: 6,
+  sunday: 7,
+};
+
+function findNextOccurrenceOfWeekday(start, weekday) {
+  const ISOValue = weekdayToISO[weekday];
+  if(start.weekday() > ISOValue) {
+    return start.clone().add(1, 'week').day(ISOValue);
+  } else {
+    return start.clone().day(ISOValue);
+  }
+}
+
+export default function Schedule({
+  selectedDate,
+  weekdays,
+  occurrences,
+}) {
+  const classes = useStyles();
+
+  const weekdaysToSchedule = [];
+  for (let [key, value] of Object.entries(weekdays)) {
+    if (value) {
+      weekdaysToSchedule.push(key);
+    }
+  }
+
+  if (!weekdaysToSchedule.length || occurrences < 1) {
+    return null;
+  }
+
+  // Find the day to start with
+  // Start with the day of the selected date if possible
+  // Otherwise move forward in the week
+  // If the end of the week is reached, default to the first position
+  let dayToFind = Number(selectedDate.format('E'));
+  let startingPosition = 0;
+
+  for (let ii = dayToFind; ii < 8; ii++) {
+    const positionOfDay = weekdaysToSchedule.indexOf(ISOToWeekday[ii]);
+    if (positionOfDay > -1) {
+      startingPosition = positionOfDay;
+      break;
+    }
+  }
+
+  const firstDay = ISOToWeekday[selectedDate.format('E')];
+  const firstDayPosition = weekdaysToSchedule.indexOf(firstDay);
+
+  const schedule = [];
+  for (let ii = 0; ii < occurrences; ii++) {
+    if (ii === 0 && startingPosition === firstDayPosition) {
+      // Start with the selected date
+      schedule.push(selectedDate);
+    } else {
+      const weekday = weekdaysToSchedule[(startingPosition + ii) % weekdaysToSchedule.length];
+      if (ii === 0) {
+        // Use the next occurrence of the week day, after the selected date
+        // schedule.push(selectedDate.calendar())
+        schedule.push(findNextOccurrenceOfWeekday(selectedDate, weekday));
+      } else {
+        // Use the next occurrence of the week day, after the last date
+        schedule.push(findNextOccurrenceOfWeekday(schedule[ii - 1], weekday));
+      }
+    }
+  }
+  return (
+    <Box display="flex" flexDirection="column" alignItems="flex-start">
+      <Typography variant="h6" className={classes.title}>
+        Schedule
+      </Typography>
+      <Typography variant="body1" align="left">
+        {schedule.map((day, index) => (<>{day.format('dddd, M/D h:mm A')}<br /></>))}
+      </Typography>
+    </Box>
+  );
+}
